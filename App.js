383916602666c4976f5b76409b4449212ea4a07c
nhsenthilkumar <senthilkumar.nh@mail.weir>
2019-12-15 10:22:18 +0530

    added few component for 2 letter word

--- App.js
+++ App.js
@@ -1,63 +1,60 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
+import React from 'react';
+import { Container, Header, Content, Left, Body, Right, Card, CardItem, Title, Text } from 'native-base';
+import LettersSound from './phonetics/lettersound';
+import TwoLettersSound from './phonetics/twolettersound';
+import VowelsSound from './phonetics/vowelssound';
+import * as Font from 'expo-font'
 
-
-
-export default class App extends Component {
-  state = { currentletter: '' };
- constructor(props) {
+export default class App extends React.Component {
+  constructor(props) {
     super(props);
-    this.onclick = this.onclick.bind(this);
+    
   }
-  onclick(){ 
-    const random1 = random(0, 1);
-    const vowelsSelector = parseInt(random(0, 4));
-    const consonentsSelector = parseInt(random(0, 20))
-    const currentVowel = vowels[vowelsSelector];
-    const currentConsonent = consonents[consonentsSelector];
-    let newString = '';
 
-    if(random1 < 0.5){
-      newString = `${currentConsonent}${currentVowel}`;
-    }else{
-      newString = `${currentVowel}${currentConsonent}`;
-    }
-    this.setState({currentletter: newString})
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    }); 
   }
-  render(){
+
+  render() {
+   
+
     return (
-      <View style={{flex: 1}}>
-      <TouchableNativeFeedback
-              onPress={this.onclick}
-              background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
-              
-              <View style={{flex: 1, padding: 20, backgroundColor: 'blue'}}>
-              <Text style={styles.titleText}>{this.state.currentletter}</Text>
-             </View>
-                  
-              </TouchableNativeFeedback>
-      </View>
-      
-     
-     
+      <Container>
+        <Header>
+          <Left></Left>
+          <Body>
+            <Title><Text>Welcome Pranaav</Text></Title>
+          </Body>
+          <Right></Right>
+        </Header>
+        <Content>
+          
+          <Card>
+            <CardItem header>
+              <Text>Letter Sounds</Text>
+            </CardItem>
+              <Body>
+                <LettersSound></LettersSound>
+              </Body>
+            <CardItem>
+            </CardItem>
+          </Card>
+          <Card>
+          <CardItem header>
+            <Text>2 Letter Words</Text>
+          </CardItem>
+            <Body>
+              <TwoLettersSound></TwoLettersSound>
+            </Body>
+          <CardItem>
+          </CardItem>
+        </Card>
+        </Content>
+      </Container>
     );
   }
 }
-
-const vowels = ['a', 'e', 'i', 'o', 'u'];
-const consonents = ['b','c','d','f','g','h','j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v','w','x','y', 'z']
-const random = (min, max) => Math.random() * (+max - +min) + +min;
-const styles = StyleSheet.create({
-  baseText: {
-    fontFamily: 'Cochin',
-  },
-  titleText: {
-    flex: 1,
-    justifyContent: 'center',
-    fontSize:800,
-    alignItems: 'center', 
-    fontWeight: 'bold',
-  },
-});
-
-
---
+++ alphabets.js
@@ -0,0 +1,9 @@
+const consonant = ['b','c','d','f','g','h','j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v','w','x','y', 'z']
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const alphabets = ['a','b','c','d','e','f','g','h','i','j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 's', 't', 'u', 'v','w','x','y', 'z']
+
+export default {
+    consonant: consonant,
+    vowels: vowels,
+    alphabets: alphabets
+}
